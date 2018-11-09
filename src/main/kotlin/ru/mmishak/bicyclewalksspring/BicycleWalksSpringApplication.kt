b4dfd79b59f82c12358cbb4019fc2e381c8ac30d
Mikhail Kozlov <mmishak.ya@gmail.com>
2018-11-09 15:05:18 +0300

    spring init

---
+++ src/main/kotlin/ru/mmishak/bicyclewalksspring/BicycleWalksSpringApplication.kt
@@ -0,0 +1,11 @@
+package ru.mmishak.bicyclewalksspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BicycleWalksSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<BicycleWalksSpringApplication>(*args)
+}
